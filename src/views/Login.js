dc272a4db855bc7028d294592d9aace0eb9a1e83
Stoica Tedy <stoicatedy@gmail.com>
2020-05-28 15:15:48 +0300

    asd

--- src/views/Login.js
+++ src/views/Login.js
@@ -64,7 +64,7 @@ export default function Login(props) {
           }),
         } ).then(res => res.json()).then(data => {
           if(data.success === true) {
-            props.mainUpdate(true);
+            props.mainUpdate(false);
           }
         });
       }
