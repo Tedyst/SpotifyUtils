29b7956e48eb0486bac9b9b5e5fbb4d8e01697ef
Stoica Tedy <stoicatedy@gmail.com>
2020-06-01 10:47:59 +0300

    asd

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM python:3.7
+
 WORKDIR /app/api
+VOLUME /data
+
 COPY api/requirements.txt /app/api/requirements.txt
 RUN ["pip", "install", "-r", "/app/api/requirements.txt"]
+
 CMD ["flask", "run", "--host", "0.0.0.0"]
+ENV APP_ENV=docker
+
 COPY api/ /app/api
 COPY build/ /app/build
--- api/SpotifyUtils/__init__.py
+++ api/SpotifyUtils/__init__.py
@@ -4,13 +4,23 @@ from flask_login import LoginManager
 from logging import INFO, DEBUG
 import SpotifyUtils.config as config
 from flask_migrate import Migrate
+import ptvsd
+import os
 
 APP = Flask(__name__,
             static_folder="../../build/static")
 APP.config['SECRET_KEY'] = config.SECRET_KEY
 APP.config['SQLALCHEMY_ECHO'] = False
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data.db'
+
+if os.getenv("APP_ENV") == "docker":
+    APP.logger.info("Enabled vscode debugger")
+    ptvsd.enable_attach()
+
+    APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/data.db'
+else:
+    APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data.db'
+
 if APP.debug:
     APP.logger.setLevel(DEBUG)
 else:
