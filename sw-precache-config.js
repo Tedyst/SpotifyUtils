ff9532081d9da520a29b59a8aba498f78b39fe7e
Stoica Tedy <stoicatedy@gmail.com>
2020-06-16 12:25:53 +0300

    added serviceworker that works ?

---
+++ sw-precache-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    staticFileGlobs: [
+        'build/*.html',
+        'build/manifest.json',
+        'build/static/**/!(*map*)'
+    ],
+    staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    swFilePath: './build/service-worker.js',
+    stripPrefix: 'build/',
+    runtimeCaching: [{
+        urlPattern: /admin/,
+        handler: 'networkOnly'
+    }, {
+        urlPattern: /api/,
+        handler: 'networkOnly'
+    }]
+}
