6e6729a5bc2f31a78235425dd19192559f732d90
Stoica Tedy <stoicatedy@gmail.com>
2020-06-20 18:35:19 +0300

    added better config for this particular file

--- api/SpotifyUtils/functions/PlaylistFunctions/SavePlaylist.py
+++ api/SpotifyUtils/functions/PlaylistFunctions/SavePlaylist.py
@@ -1,15 +1,17 @@
 from SpotifyUtils.user import User
 import spotipy
 
+PLAYLIST_NAME = "Spotify Utils Playlist"
+
 
 def SavePlaylist(target: User, tracks):
     sp = spotipy.Spotify(target.token)
     playlist_id = None
     user_playlists = sp.current_user_playlists()
     for playlist in user_playlists["items"]:
-        if playlist["name"] == "Spotify Utils Playlist":
+        if playlist["name"] == PLAYLIST_NAME:
             playlist_id = playlist["uri"]
     if playlist_id is None:
         playlist_id = sp.user_playlist_create(
-            target.username, "Spotify Utils Playlist")["uri"]
+            target.username, PLAYLIST_NAME)["uri"]
     sp.user_playlist_replace_tracks(target.username, playlist_id, tracks)
