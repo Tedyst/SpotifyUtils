f2259421638de75d6fcd371e6f3557404168786d
Stoica Tedy <stoicatedy@gmail.com>
2020-06-20 18:43:38 +0300

    removed some logging

--- api/SpotifyUtils/functions/Top.py
+++ api/SpotifyUtils/functions/Top.py
@@ -15,7 +15,6 @@ def Top(user: User):
         result["artists"] = json.loads(user.top_artists)
         result["tracks"] = json.loads(user.top_tracks)
         result["genres"] = json.loads(user.top_genres)
-        APP.logger.debug("Token expired for user %s", user.name)
         APP.logger.debug("Loaded from database for %s", user.name)
         return result
     sp = spotipy.Spotify(user.token)
