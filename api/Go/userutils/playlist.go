bb1a555da5981780f55b37d6ae602bf7f9f6cdec
Stoica Tedy <stoicatedy@gmail.com>
2020-12-08 08:02:05 +0200

    sdf

--- api/Go/userutils/playlist.go
+++ api/Go/userutils/playlist.go
@@ -2,24 +2,23 @@ package userutils
 
 import (
 	"github.com/tedyst/spotifyutils/api/logging"
-	"github.com/tedyst/spotifyutils/api/tracks"
 	"github.com/zmb3/spotify"
 )
 
-func (u *User) GetPlaylist(ID string) []spotify.SimpleTrack {
+func (u *User) GetPlaylist(ID string) *spotify.FullPlaylist {
 	items, err := u.Client.GetPlaylist(spotify.ID(ID))
 	if err != nil {
 		logging.ReportError("userutils.GetPlaylist()", err)
-		return []spotify.SimpleTrack{}
+		return &spotify.FullPlaylist{}
 	}
 
-	// Preloading the tracks in memory and updating them just in case they are used
-	go func(it []spotify.SimpleTrack) {
-		for _, s := range it {
-			t := tracks.RecentlyPlayedItemToTrack(s.Track)
-			t.Update(u.Client)
-		}
-	}(items)
+	// // Preloading the tracks in memory and updating them just in case they are used
+	// go func(it []spotify.SimpleTrack) {
+	// 	for _, s := range it {
+	// 		t := tracks.RecentlyPlayedItemToTrack(s)
+	// 		t.Update(u.Client)
+	// 	}
+	// }(items)
 
 	return items
 }
