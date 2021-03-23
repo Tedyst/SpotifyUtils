f58044de90c865f28a3ab67fe7d2d7cacdea6df2
Stoica Tedy <stoicatedy@gmail.com>
2021-03-23 16:00:50 +0200

    added "time" if count is 1

--- frontend/src/components/SongCardUp.stories.js
+++ frontend/src/components/SongCardUp.stories.js
@@ -3,15 +3,24 @@ import SongCardUp from './SongCardUp';
 
 const Template = (args) => <SongCardUp {...args} usingLink={false} />;
 
-export const WithCount = Template.bind({});
+export const WithCountOne = Template.bind({});
+export const WithCountTwo = Template.bind({});
 export const WithoutCount = Template.bind({});
 
-WithCount.args = {
+WithCountOne.args = {
     uri: 'test',
     image: 'https://i.scdn.co/image/ab67616d0000b273309b0cb81728d42a6dfb2b81',
     name: 'Forever ... (is a long time)',
     artist: 'Halsey',
     count: '1',
+};
+
+WithCountTwo.args = {
+    uri: 'test',
+    image: 'https://i.scdn.co/image/ab67616d0000b273309b0cb81728d42a6dfb2b81',
+    name: 'Forever ... (is a long time)',
+    artist: 'Halsey',
+    count: '2',
 };
 
 WithoutCount.args = {
