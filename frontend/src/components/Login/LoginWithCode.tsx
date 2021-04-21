a096a4c947b445abf28cf436ae78e7e708f96531
Stoica Tedy <stoicatedy@gmail.com>
2021-04-21 12:55:41 +0300

    removed /status spam

--- frontend/src/components/Login/LoginWithCode.tsx
+++ frontend/src/components/Login/LoginWithCode.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery, useQueryClient } from 'react-query';
 import axios from 'axios';
 import LoginPage from '../Login/LoginPage';
@@ -14,17 +14,19 @@ export default function LoginWithCode(props: { code: string, CSRFToken: string }
             'X-CSRF-Token': props.CSRFToken,
         },
     }));
+    useEffect(() => {
+        if (data?.data.success) {
+            queryClient.invalidateQueries('status');
+            queryClient.invalidateQueries('top');
+        }
+    }, [data]);
     if (isLoading) {
         return <LoginPage loggingIn />;
     }
     if (error) {
         return <LoginPage loggingIn={false} />;
     }
-    if (data?.data.success) {
-        queryClient.invalidateQueries('status');
-        queryClient.invalidateQueries('top');
-        return <LoginPage loggingIn />;
-    }
+
     return <LoginPage loggingIn />;
 }
 
