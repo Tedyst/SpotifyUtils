75b0d8e577a0bf8cf2e2d2bd0451c8572a3afa8e
Stoica Tedy <stoicatedy@gmail.com>
2021-04-21 13:18:32 +0300

    fixed caching

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -30,6 +30,7 @@ const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+            staleTime: 1000 * 60 * 60 * 24, // 24 hours
         },
     },
 });
