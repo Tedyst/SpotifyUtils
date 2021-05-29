4554438aa228b578ce9fb3ae57c0b846230cc430
Stoica Tedy <stoicatedy@gmail.com>
2021-05-29 15:08:05 +0300

    added more tests

--- frontend/src/tests/setupTests.js
+++
@@ -1,9 +0,0 @@
-// setupTests.js
-import { format } from 'util';
-
-const { error } = global.console;
-
-global.console.error = function (...args) {
-    error(...args);
-    throw new Error(format(...args));
-};
