20cfabc9213498c73f560c145688bc6fa323830e
Stoica Tedy <stoicatedy@gmail.com>
2021-06-12 09:03:40 +0300

    you learn smth new every day

--- tracks/lyrics/genius/genius.go
+++ tracks/lyrics/genius/genius.go
@@ -26,22 +26,19 @@ func Lyrics(trackName string, trackArtist string) (string, error) {
 	if *config.MockExternalCalls {
 		return "", errors.New("getting song info from Genius is disabled")
 	}
+	contextLogger := log.WithFields(log.Fields{
+		"type":   "genius",
+		"name":   trackName,
+		"artist": trackArtist,
+	})
 	name := fmt.Sprintf("%s %s", trackArtist, trackName)
 	res, err := config.GeniusClient.Search(name)
 	if err != nil {
-		log.WithFields(log.Fields{
-			"type":   "genius",
-			"name":   trackName,
-			"artist": trackArtist,
-		}).Error(err)
+		contextLogger.Error(err)
 		return "", err
 	}
 	if len(res.Response.Hits) == 0 {
-		log.WithFields(log.Fields{
-			"type":   "genius",
-			"name":   trackName,
-			"artist": trackArtist,
-		}).Debug("Could not find song on Genius")
+
 		return "", errors.New("could not find song on Genius")
 	}
 	for _, s := range res.Response.Hits {
@@ -50,42 +47,29 @@ func Lyrics(trackName string, trackArtist string) (string, error) {
 			for i := 1; i <= retryCount; i++ {
 				lyrics, err = getLyricsFromURL(s.Result.URL)
 				if err != nil {
-					log.WithFields(log.Fields{
-						"type":   "genius",
-						"url":    s.Result.URL,
-						"name":   trackName,
-						"artist": trackArtist,
+					contextLogger.WithFields(log.Fields{
+						"url": s.Result.URL,
 					}).Error(err)
 					break
 				}
 				if lyrics != "" {
 					return lyrics, nil
 				} else {
-					log.WithFields(log.Fields{
-						"type":   "genius",
-						"url":    s.Result.URL,
-						"name":   trackName,
-						"artist": trackArtist,
+					contextLogger.WithFields(log.Fields{
+						"url": s.Result.URL,
 					}).Debug("Trying again")
 					time.Sleep(retryTimeout)
 				}
 			}
 			if lyrics == "" {
-				log.WithFields(log.Fields{
-					"type":   "genius",
-					"url":    s.Result.URL,
-					"name":   trackName,
-					"artist": trackArtist,
+				contextLogger.WithFields(log.Fields{
+					"url": s.Result.URL,
 				}).Debug("Could not extract lyrics")
 			}
 			break
 		}
 	}
-	log.WithFields(log.Fields{
-		"type":   "genius",
-		"name":   trackName,
-		"artist": trackArtist,
-	}).Debug("Could not find Lyrics")
+	contextLogger.Debug("Could not find Lyrics")
 	return "", errors.New("could not find Genius Lyrics")
 }
 
@@ -109,8 +93,12 @@ func getLyricsFromURL(url string) (string, error) {
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Errorf("status code error: %d %s", res.StatusCode, res.Status)
-		return "", fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+		err := fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+		log.WithFields(log.Fields{
+			"type": "genius",
+			"url":  url,
+		}).Error(err)
+		return "", err
 	}
 
 	// Load the HTML document
@@ -130,7 +118,7 @@ func getLyricsFromURL(url string) (string, error) {
 			lyrics += text + "\n"
 		}
 	})
-	strings.ReplaceAll(lyrics, "\n\n", "\n")
+	lyrics = strings.ReplaceAll(lyrics, "\n\n", "\n")
 	lyrics = stripText(lyrics)
 	return lyrics, nil
 }
