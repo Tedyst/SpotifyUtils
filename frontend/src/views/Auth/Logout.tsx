f143711e98f905ab220cab2bd42972f8873b2c57
Stoica Tedy <stoicatedy@gmail.com>
2021-06-15 14:30:18 +0300

    changed logout to use axios

--- frontend/src/views/Auth/Logout.tsx
+++ frontend/src/views/Auth/Logout.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
 import { QueryClient } from 'react-query';
 import Loading from '../../components/Loading';
@@ -6,7 +7,7 @@ export default function Logout() {
     const queryClient = new QueryClient();
 
     useEffect(() => {
-        fetch('/api/logout', { cache: 'no-store', credentials: 'same-origin' }).then(() => {
+        axios.get('/api/logout').then(() => {
             localStorage.clear();
             queryClient.invalidateQueries();
             window.location.reload();
