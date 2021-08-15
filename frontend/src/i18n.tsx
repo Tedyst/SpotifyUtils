098b4751730fb152e3c3065c79cff3342d4ab630
Stoica Tedy <stoicatedy@gmail.com>
2021-08-15 13:51:19 +0300

    fixed debug mode only on dev

--- frontend/src/i18n.tsx
+++ frontend/src/i18n.tsx
@@ -14,7 +14,7 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
-        debug: true,
+        debug: process.env.NODE_ENV === 'development',
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
