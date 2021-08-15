f32c825406a62865d34f5b1177ae798e197c1673
Stoica Tedy <stoicatedy@gmail.com>
2021-08-15 13:28:19 +0300

    made chart2 responsive

--- frontend/src/components/Track/Chart2.tsx
+++ frontend/src/components/Track/Chart2.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Paper, Typography } from '@material-ui/core';
+import {
+    Paper,
+    Typography,
+    useMediaQuery,
+    useTheme,
+} from '@material-ui/core';
 import {
     BarSeries,
     ArgumentAxis,
@@ -26,6 +31,73 @@ export default function Chart2(props: {
         speechiness,
     } = props;
     const { t } = useTranslation();
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down('sm'));
+    if (matches) {
+        const data1 = [
+            {
+                lineValue: acousticness,
+                argument: t('TRACK.ACOUSTICNESS'),
+            },
+            {
+                lineValue: danceability,
+                argument: t('TRACK.DANCEABILITY'),
+            },
+            {
+                lineValue: energy,
+                argument: t('TRACK.ENERGY'),
+            },
+            {
+                lineValue: instrumentalness,
+                argument: t('TRACK.INSTRUMENTALNESS'),
+            },
+        ];
+        const data2 = [
+            {
+                lineValue: liveness,
+                argument: t('TRACK.LIVENESS'),
+            },
+            {
+                lineValue: loudness,
+                argument: t('TRACK.LOUDNESS'),
+            },
+            {
+                lineValue: speechiness,
+                argument: t('TRACK.SPEECHINESS'),
+            },
+        ];
+        return (
+            <>
+                <Typography>
+                    {t('TRACK.TRACK_FEATURES')}
+                </Typography>
+                <Paper>
+                    <Chart
+                        data={data1}
+                        height={120}
+                    >
+                        <BarSeries
+                            argumentField="argument"
+                            valueField="lineValue"
+                        />
+                        <ArgumentAxis />
+                    </Chart>
+                    <br />
+                    <Chart
+                        data={data2}
+                        height={120}
+                    >
+                        <BarSeries
+                            argumentField="argument"
+                            valueField="lineValue"
+                        />
+                        <ArgumentAxis />
+                    </Chart>
+                </Paper>
+            </>
+        );
+    }
+
     const data = [
         {
             lineValue: acousticness,
