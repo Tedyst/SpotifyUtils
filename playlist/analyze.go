952f64e94222d0e08f17ddc8d27719c4b5d19af5
Stoica Tedy <stoicatedy@gmail.com>
2021-08-23 20:37:18 +0300

    feat(backend): analyze playlist based on its tracks

--- playlist/analyze.go
+++ playlist/analyze.go
@@ -1,28 +1,155 @@
 package playlist
 
-import "github.com/tedyst/spotifyutils/tracks"
+import (
+	"github.com/tedyst/spotifyutils/tracks"
+)
+
+type responseSong struct {
+	Name   string
+	Artist string
+	URI    string
+	Image  string
+}
 
 type medianStruct struct {
-	Least  *tracks.Track
-	Most   *tracks.Track
-	Median int32
+	Highest struct {
+		Track responseSong
+		Value float32
+	}
+	Lowest struct {
+		Track responseSong
+		Value float32
+	}
+	Median float32
 }
 
 type AnalyzeStruct struct {
 	Artists map[string]int32
 
-	Danceability     medianStruct
 	Energy           medianStruct
 	Acousticness     medianStruct
 	Instrumentalness medianStruct
+	Popularity       medianStruct
+	Tempo            medianStruct
 
-	Genres     []string
-	Popularity medianStruct
-	BPM        medianStruct
-	Explicit   int32
+	Genres   map[string]int32
+	Explicit int32
 }
 
 func Analyze(tracks []*tracks.Track) AnalyzeStruct {
 	response := AnalyzeStruct{}
+	response.Artists = make(map[string]int32)
+	response.Genres = make(map[string]int32)
+	for _, s := range tracks {
+		for _, artist := range s.Artists {
+			response.Artists[artist.Name] += 1
+			for _, genre := range artist.Genres {
+				response.Genres[genre] += 1
+			}
+		}
+		if response.Energy.Highest.Value < s.Information.TrackFeatures.Energy {
+			response.Energy.Highest.Value = s.Information.TrackFeatures.Energy
+			response.Energy.Highest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		if response.Energy.Lowest.Value > s.Information.TrackFeatures.Energy || response.Energy.Lowest.Value == 0 {
+			response.Energy.Lowest.Value = s.Information.TrackFeatures.Energy
+			response.Energy.Lowest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		response.Energy.Median += s.Information.TrackFeatures.Energy
+		if response.Acousticness.Highest.Value < s.Information.TrackFeatures.Acousticness {
+			response.Acousticness.Highest.Value = s.Information.TrackFeatures.Acousticness
+			response.Acousticness.Highest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		if response.Acousticness.Lowest.Value > s.Information.TrackFeatures.Acousticness || response.Acousticness.Lowest.Value == 0 {
+			response.Acousticness.Lowest.Value = s.Information.TrackFeatures.Acousticness
+			response.Acousticness.Lowest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		response.Acousticness.Median += s.Information.TrackFeatures.Acousticness
+		if response.Instrumentalness.Highest.Value < s.Information.TrackFeatures.Instrumentalness {
+			response.Instrumentalness.Highest.Value = s.Information.TrackFeatures.Instrumentalness
+			response.Instrumentalness.Highest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		if response.Instrumentalness.Lowest.Value > s.Information.TrackFeatures.Instrumentalness || response.Instrumentalness.Lowest.Value == 0 {
+			response.Instrumentalness.Lowest.Value = s.Information.TrackFeatures.Instrumentalness
+			response.Instrumentalness.Lowest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		response.Instrumentalness.Median += s.Information.TrackFeatures.Instrumentalness
+		if response.Popularity.Highest.Value < float32(s.Information.TrackInformation.Popularity) {
+			response.Popularity.Highest.Value = float32(s.Information.TrackInformation.Popularity)
+			response.Popularity.Highest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		if response.Popularity.Lowest.Value > float32(s.Information.TrackInformation.Popularity) || response.Popularity.Lowest.Value == 0 {
+			response.Popularity.Lowest.Value = float32(s.Information.TrackInformation.Popularity)
+			response.Popularity.Lowest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		response.Popularity.Median += float32(s.Information.TrackInformation.Popularity)
+		if response.Tempo.Highest.Value < float32(s.Information.TrackInformation.Tempo) {
+			response.Tempo.Highest.Value = float32(s.Information.TrackInformation.Tempo)
+			response.Tempo.Highest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		if response.Tempo.Lowest.Value > float32(s.Information.TrackInformation.Tempo) || response.Tempo.Lowest.Value == 0 {
+			response.Tempo.Lowest.Value = float32(s.Information.TrackInformation.Tempo)
+			response.Tempo.Lowest.Track = responseSong{
+				Name:   s.Name,
+				Artist: s.ArtistString(),
+				URI:    s.TrackID,
+				Image:  s.Information.TrackInformation.Image,
+			}
+		}
+		response.Tempo.Median += float32(s.Information.TrackInformation.Tempo)
+		if s.Information.TrackInformation.Explicit {
+			response.Explicit += 1
+		}
+	}
+	response.Energy.Median /= float32(len(tracks))
+	response.Acousticness.Median /= float32(len(tracks))
+	response.Instrumentalness.Median /= float32(len(tracks))
+	response.Popularity.Median /= float32(len(tracks))
+	response.Tempo.Median /= float32(len(tracks))
 	return response
 }
