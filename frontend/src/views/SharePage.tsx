3771d3d8b6e26ac14df3649408ea9f7603960e4e
Stoica Tedy <stoicatedy@gmail.com>
2021-08-23 18:17:27 +0300

    feat(frontend): add alert if share url is invalid

--- frontend/src/views/SharePage.tsx
+++ frontend/src/views/SharePage.tsx
@@ -1,23 +1,39 @@
+import { Container } from '@material-ui/core';
+import { Alert, AlertTitle } from '@material-ui/lab';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import {
     Redirect,
 } from 'react-router-dom';
+import Top from './TopPage';
 
 export default function Share() {
     const parsedUrl = new URLSearchParams(window.location.search).get('text');
+    const { t } = useTranslation();
+    const wrongComp = (
+        <>
+            <Container maxWidth="xs">
+                <Alert severity="error">
+                    <AlertTitle>{t('COMMON.INVALID_SHARE_LINK')}</AlertTitle>
+                </Alert>
+            </Container>
+            <br />
+            <Top />
+        </>
+    );
     if (parsedUrl === null) {
-        return <Redirect to="/" />;
+        return wrongComp;
     }
     try {
         const url = new URL(parsedUrl);
         if (url.hostname !== 'open.spotify.com') {
-            return <Redirect to="/" />;
+            return wrongComp;
         }
         const path = url.pathname.replace('/track/', '');
         return (
             <Redirect to={`/track/${path}`} />
         );
     } catch (_) {
-        return <Redirect to="/" />;
+        return wrongComp;
     }
 }
